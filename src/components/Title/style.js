17d17268c8ff4425f61d28caf4cc0afa08489828
Davi Nascimento <nascimentoos.davi@gmail.com>
2024-07-27 15:36:54 -0300

    v1.2 - design

---
+++ src/components/Title/style.js
@@ -0,0 +1,21 @@
+import { StyleSheet} from "react-native";
+import Title from ".";
+
+
+const styles = StyleSheet.create({
+    boxTitle:{
+        padding:10,
+        justifyContent:'center',
+        alignItems: 'center',
+
+    },
+    textTitle:{
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: '#2fafff'
+    },
+});
+
+export default styles;
+
+
